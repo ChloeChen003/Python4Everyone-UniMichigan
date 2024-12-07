23d2ec0071fc4008f2506de3181b8648af09d7b1
Chloe Chen <onechen23@gmail.com>
2024-12-07 13:32:46 +1100

    updates

---
+++ C1. Programming for everyone/C1. codes.py
@@ -0,0 +1,36 @@
+
+# Variable and Key words 
+76trombones = 'big parade' # begins with a number 
+more@ = 1000000 # contains illegal character 
+class = 'Advanced Theoretical Zymurgy' # class is one of Python's keywords 
+
+first =10
+second =15
+print(first + second)
+
+for word in words:
+    print(word)
+
+# 2.15 Exercises
+a = input("Hello ")
+print(a)
+
+#3. 
+Hours = 35
+Rate = 2.75
+Pay = Hours * Rate
+print(Pay)
+
+# 4. 
+width = 17
+height = 12.0
+Width//2
+width/2.0
+height/3
+1+2*5
+
+# 5. Write a program which prompts the user for a Celsius temperature,
+convert the temperature to Fahrenheit, and print out the converted temperature
+Celtemp = input("Current Celsius temperature is: ")
+Fahtemp = int(Celtemp) * 9 / 5 +32
+print(Fahtemp)
